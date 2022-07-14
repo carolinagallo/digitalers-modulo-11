0917c50329b29009495f1b4d65f5599cd05a32f4
carolinagallo <carolinagallo02@gmail.com>
2022-07-14 15:22:51 -0300

    primer commit

---
+++ desafio 1/js/apps.js
@@ -0,0 +1,6 @@
+let fijo=prompt('Ingrese el valor de los costos fijos')
+let venta=prompt('Ingrese el precio de venta')
+let variable=prompt('Ingrese el valor de los costos variables')
+
+let equilibrio= (venta-variable)/fijo
+document.write("El punto de equilibrio es: ", equilibrio)
---
+++ desafio 2/js/apps.js
@@ -0,0 +1,5 @@
+let salario=prompt('Ingrese su salario mensual')
+let gastosnec=salario/2
+let gastosop=salario*(30/100)
+let ahorro=salario*(20/100)
+document.write(' ─Para gastos necesarios: ',gastosnec,' ─Para gastos opcionales: ',gastosop,' ─Para ahorrar: ',ahorro)
